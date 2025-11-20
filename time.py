7e6c427612522f436f75b8601d33dfc6a304db67
hannasherin-261 <ansariashiq215@gmail.com>
2025-11-20 14:51:49 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,6 @@
+import time
+import datetime
+print("current date and time:",datetime.datetime.now())
+print("current year:",datetime.date.today().strftime("%Y"))
+
+print("month of year:",datetime.date.today().strftime("%B"))
